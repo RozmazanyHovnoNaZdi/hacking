c29a06f8fd5878c4fdd32313aec7619b16c828a7
Jakub Matějka <jakub.matejka@educhem.cz>
2022-02-28 13:24:43 +0100

    Přidejte soubory projektu.

---
+++ hacking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace hacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("!hacking = žielenskýOnlyFans...");
+            for (int i = 0; i <= 100; i = i + 5)
+            {
+                Console.WriteLine($"progress: {i}%");
+                Thread.Sleep(3700);
+            }
+            Console.WriteLine("password hack successful >> putting backdoor (for later informational purposes)");
+            for (int i = 0; i <= 100; i = i + 25)
+            {
+                Console.WriteLine($"backdoor implantation: {i}%");
+                Thread.Sleep(1400);
+            }
+            Console.WriteLine("hack finished! password = (gimpmamina123)and backdoor planted");
+
+        }
+    }
+}
